449277638ee6f94dcd0f47de25c8e259afec6a47
Ista07-ui <i.c.k.85995@gmail.com>
2026-03-28 10:33:34 +0700

    style: Format Food interface properties for consistency

--- src/app/types/food.ts
+++ src/app/types/food.ts
@@ -1,14 +1,14 @@
 export type FoodType = "upf" | "fresh";
 
 export interface Food {
-	id: string;
-	name: string;
-	description: string;
-	imageUrl: string;
-	ingredients: string;
-	type: FoodType;
-	price?: number | null;
-	createdAt: Date;
-	updatedAt: Date;
-	deletedAt?: Date | null;
+  id: string;
+  name: string;
+  description: string;
+  imageUrl: string;
+  ingredients: string;
+  type: FoodType;
+  price?: number | null;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date | null;
 }
